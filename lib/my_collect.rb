f150fc72be0a10593842374c2d5207069e594ee3
Anne Fausto <a.r.fausto@gmail.com>
2019-09-23 02:07:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,10 @@ def my_collect(array)
   counter = 0 
     my_collect(empty_array) do |x|
       counter += 1 
-      
+    end
+  end 
+  
+  
   my_collect(collection) do |lang| 
     lang.upcase
     
